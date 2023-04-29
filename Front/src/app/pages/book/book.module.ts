9fe0dc7c04ca1bb7834ee8c3dce5c74ab4d37bb4
Jesica Aramayo <jess.ara95@outlook.com>
2023-04-29 20:20:57 -0300

    add book-detail-page component and pages.module config

---
+++ Front/src/app/pages/book/book.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BookDetailComponent } from './book-detail-page/book-detail.component';
+
+
+
+@NgModule({
+  declarations: [
+    BookDetailComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BookModule { }
