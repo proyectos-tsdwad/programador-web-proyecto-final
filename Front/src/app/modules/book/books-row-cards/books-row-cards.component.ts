ad35501afd06e6beced898c1982b2e0bda6d1bf0
carlos linero <lineroacostacarlos@gmail.com>
2023-04-29 17:56:36 -0300

    add book card and row cards components

---
+++ Front/src/app/modules/book/books-row-cards/books-row-cards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-books-row-cards',
+  templateUrl: './books-row-cards.component.html',
+  styleUrls: ['./books-row-cards.component.css']
+})
+export class BooksRowCardsComponent {
+
+}
