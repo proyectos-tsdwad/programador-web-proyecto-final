91c09888540f2fc3638642beefd17f32bbe71d9f
carlos linero <lineroacostacarlos@gmail.com>
2023-04-29 18:05:36 -0300

    add book, author and publisher models

---
+++ Front/src/app/models/book/book-model.ts
@@ -0,0 +1,19 @@
+import { Author } from "../author/author-model";
+import { Publisher } from "../publisher/publisher-model";
+
+export interface Book {
+  title: string;
+  author: Author;
+  img: string;
+  description: string;
+  publisher: Publisher;
+  isbn: string;
+  pageAmount: number;
+  language: string;
+  genre: string;
+  releaseDate: Date;
+  score: number;
+  stock: number;
+  rentedTotal: number;
+  tags: string[];
+}
