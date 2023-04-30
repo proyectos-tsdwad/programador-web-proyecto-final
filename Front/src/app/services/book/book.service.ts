422a205a5a2fc95a5a7cc474ba2e86657595d031
carlos linero <lineroacostacarlos@gmail.com>
2023-04-30 11:44:41 -0300

    add methods in book service

--- Front/src/app/services/book/book.service.ts
+++ Front/src/app/services/book/book.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { allBooks } from './book-simulation-data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
+  books = [...allBooks];
+
   constructor() { }
+
+  getAllBooks() {
+    return this.books;
+  }
+
+  // getRecomendedBooks() {
+  //   return this.books.filter(book =>{
+  //     book.tags.includes('tag');
+  //   });
+  // }
 }
