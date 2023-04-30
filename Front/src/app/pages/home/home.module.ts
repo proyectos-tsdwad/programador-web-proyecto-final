5fd5584aa6805430dd5e78c160b65ba2a720b7fe
carlos linero <lineroacostacarlos@gmail.com>
2023-04-30 10:56:44 -0300

    add img assets

--- Front/src/app/pages/home/home.module.ts
+++ Front/src/app/pages/home/home.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component'
 import { HeaderComponent } from './header/header.component';
-import { BookModule } from '../book/book.module';
+import { BookModule } from '../../modules/book/book.module';
 
 
 @NgModule({
