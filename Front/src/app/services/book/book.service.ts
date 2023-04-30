9421b2ecfb3fcab215135eaae6dc4a0ac6bd2835
carlos linero <lineroacostacarlos@gmail.com>
2023-04-30 12:44:07 -0300

    add basic filter methods for demo in book service

--- Front/src/app/services/book/book.service.ts
+++ Front/src/app/services/book/book.service.ts
@@ -1,22 +1,41 @@
 import { Injectable } from '@angular/core';
 import { allBooks } from './book-simulation-data';
+import { TAG } from 'src/app/utils/enums/book.enum';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  books = [...allBooks];
+  private books = allBooks;
 
   constructor() { }
 
   getAllBooks() {
-    return this.books;
+    return [...this.books];
   }
 
-  // getRecomendedBooks() {
-  //   return this.books.filter(book =>{
-  //     book.tags.includes('tag');
-  //   });
-  // }
+  getRecomendedBooks() {
+    const recomendedBooks = this.books.filter(book => {
+      book.tags.includes(TAG.RECOMENDADOS);
+    });
+
+    return [...recomendedBooks]
+  }
+
+  getNewAtBooks() {
+    const newAtBooks = this.books.filter(book => {
+      book.tags.includes(TAG.NOVEDADES);
+    });
+
+    return [...newAtBooks]
+  }
+
+  getTopSellerBooks() {
+    const topSellerBooks = this.books.filter(book => {
+      book.tags.includes(TAG.TOP_VENDIDOS);
+    });
+
+    return [...topSellerBooks]
+  }
 }
