a6d5c02b8f341c4ed029a941c99001180ef58c67
carlos linero <lineroacostacarlos@gmail.com>
2023-05-01 19:47:46 -0300

    change icons location

--- Front/src/app/pages/cart/selected-book-detail/selected-book-detail.component.ts
+++ Front/src/app/pages/cart/selected-book-detail/selected-book-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Book } from '../../../models/book/book-model'
 
 @Component({
   selector: 'app-selected-book-detail',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./selected-book-detail.component.css']
 })
 export class SelectedBookDetailComponent {
+
+  @Input() book!: Book;
 
 }
