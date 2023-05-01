f4b2ebdc248131d255be1e54d6b14041a35929cf
carlos linero <lineroacostacarlos@gmail.com>
2023-05-01 18:32:14 -0300

    add selected books component

--- Front/src/app/pages/cart/cart.module.ts
+++ Front/src/app/pages/cart/cart.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SelectedBooksComponent } from './selected-books/selected-books.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SelectedBooksComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ Front/src/app/pages/cart/selected-books/selected-books.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selected-books',
+  templateUrl: './selected-books.component.html',
+  styleUrls: ['./selected-books.component.css']
+})
+export class SelectedBooksComponent {
+
+}
