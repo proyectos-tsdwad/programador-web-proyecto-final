480d9978b83e2ff1747ac62cda47f9afb2669d52
carlos linero <lineroacostacarlos@gmail.com>
2023-05-01 18:49:31 -0300

    add selected books component in the Cart Module exports array declaration

--- Front/src/app/pages/cart/cart.module.ts
+++ Front/src/app/pages/cart/cart.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelectedBooksComponent } from './selected-books/selected-books.component';
+import { SelectedBookDetailComponent } from './selected-book-detail/selected-book-detail.component';
 
 
 
 @NgModule({
   declarations: [
-    SelectedBooksComponent
+    SelectedBooksComponent,
+    SelectedBookDetailComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    SelectedBooksComponent
   ]
 })
 export class CartModule { }
---
+++ Front/src/app/pages/cart/selected-book-detail/selected-book-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selected-book-detail',
+  templateUrl: './selected-book-detail.component.html',
+  styleUrls: ['./selected-book-detail.component.css']
+})
+export class SelectedBookDetailComponent {
+
+}
