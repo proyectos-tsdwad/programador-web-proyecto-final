102b143cf5e30060421e9452d347b950f8c84090
nelsonvilte <nelsonvilte@gmail.com>
2023-05-03 02:02:48 -0300

    add search component

--- Front/src/app/modules/common-layouts/common-layouts.module.ts
+++ Front/src/app/modules/common-layouts/common-layouts.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderMenuComponent } from './header-menu/header-menu.component';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
   declarations: [
     FooterComponent,
-    HeaderMenuComponent
+    HeaderMenuComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule
