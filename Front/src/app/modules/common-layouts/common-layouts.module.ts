529a541072e584ce47e2c8be1fc229d330799693
nelsonvilte <nelsonvilte@gmail.com>
2023-05-03 01:53:02 -0300

    add header-menu component

--- Front/src/app/modules/common-layouts/common-layouts.module.ts
+++ Front/src/app/modules/common-layouts/common-layouts.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
 
 
 @NgModule({
   declarations: [
-    FooterComponent
+    FooterComponent,
+    HeaderMenuComponent
   ],
   imports: [
     CommonModule
