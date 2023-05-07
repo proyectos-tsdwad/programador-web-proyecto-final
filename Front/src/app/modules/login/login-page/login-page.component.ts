7b7ee27216c3560108813b75c757879dd0d4296a
carlariossuarez <carlaariossuarez@gmail.com>
2023-05-06 23:02:35 -0300

    add redirection to register

--- Front/src/app/modules/login/login-page/login-page.component.ts
+++ Front/src/app/modules/login/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { RegisterPageComponent } from '../register-page/register-page.component';
 
 @Component({
   selector: 'app-login-page',
@@ -8,13 +9,21 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class LoginPageComponent {
 
-  constructor(public activeModal: NgbActiveModal) { }
+  constructor(
+    public activeModal: NgbActiveModal,
+    private modalService: NgbModal
+    ) { }
 
   onClickLogIn() {
     this.activeModal.close();
   }
 
   onClickClose() {
+    this.activeModal.close();
+  }
+
+  onClickEnterRegister(){
+    const modalRef = this.modalService.open(RegisterPageComponent, { fullscreen: true});
     this.activeModal.close();
   }
 
