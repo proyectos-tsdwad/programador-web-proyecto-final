2b0e9cfd6a140c66f602bb069306b5420c997b73
Katerinne Fernanda Peralta <peraltakaterinnefernanda@gmail.com>
2023-05-07 21:47:24 -0300

    app catalogue page

--- Front/src/app/pages/pages-routing.module.ts
+++ Front/src/app/pages/pages-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from "@angular/router";
 import { HomePageComponent } from "./home/home-page/home-page.component";
 import { BookDetailComponent } from "./book/book-detail-page/book-detail.component";
 import { BookDashboardPageComponent } from "./admin/book-dashboard/book-dashboard-page/book-dashboard-page.component";
+import { CataloguePageComponent } from "./catalogue/catalogue-page/catalogue-page.component";
 
 const routes: Routes = [
   { path: '', component: HomePageComponent } ,
   { path: 'home', component: HomePageComponent },
   { path: 'book-detail/:isbn', component: BookDetailComponent },
   { path: 'book-detail', component: BookDetailComponent },
-  { path: 'book-dashboard', component: BookDashboardPageComponent }
+  { path: 'book-dashboard', component: BookDashboardPageComponent },
+  { path: 'catalogue', component: CataloguePageComponent }
 ];
 
 @NgModule({
--- Front/src/app/pages/pages.module.ts
+++ Front/src/app/pages/pages.module.ts
@@ -4,6 +4,7 @@ import { HomeModule } from './home/home.module';
 import { BookDashboardModule } from './admin/book-dashboard/book-dashboard.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { BookModule } from './book/book.module';
+import { CatalogueModule } from './catalogue/catalogue.module';
 
 
 @NgModule({
@@ -14,12 +15,14 @@ import { BookModule } from './book/book.module';
     HomeModule,
     BookModule,
     BookDashboardModule,
+    CatalogueModule,
     PagesRoutingModule
   ],
   exports: [
     HomeModule,
     BookModule,
-    BookDashboardModule
+    BookDashboardModule,
+    CatalogueModule
   ]
 })
 export class PagesModule { }
