f36a15044be53e6f6aa90f523b3863fe35efff1b
carlos linero <lineroacostacarlos@gmail.com>
2023-05-07 22:57:18 -0300

    add routing service methods

--- Front/src/app/services/navigation/navigation.service.ts
+++ Front/src/app/services/navigation/navigation.service.ts
@@ -1,11 +1,50 @@
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavigationbService {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
+  navigateToHome() {
+    this.scrollTop();
+    this.router.navigate(['/home']);
+  }
+
+  navigateToBookDetail(isbn: string) {
+    this.scrollTop();
+    this.router.navigate(['/book-detail', isbn]);
+  }
+
+  navigateToBookDashBoard() {
+    this.scrollTop();
+    this.router.navigate(['/book-dashboard']);
+  }
+
+  navigateToCheckout() {
+    this.scrollTop();
+    this.router.navigate(['/checkout']);
+  }
+
+  navigateToCatalogue() {
+    this.scrollTop();
+    this.router.navigate(['catalogue']);
+  }
+
+  navigateToCartDetail() {
+    this.scrollTop();
+    this.router.navigate(['cart-detail']);
+  }
+
+  navigateToProfile() {
+    this.scrollTop();
+    this.router.navigate(['profile']);
+  }
+
+  scrollTop() {
+    window.scroll(0, 0);
+  }
 
 }
