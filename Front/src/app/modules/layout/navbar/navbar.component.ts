38ed568400618e789d2d8f440e01b94b18f70698
carlariossuarez <carlaariossuarez@gmail.com>
2023-05-21 19:17:32 -0300

    add methods to login in navbar

--- Front/src/app/modules/layout/navbar/navbar.component.ts
+++ Front/src/app/modules/layout/navbar/navbar.component.ts
@@ -1,17 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { LoginPageComponent } from '../../login/login-page/login-page.component';
 import { RegisterPageComponent } from '../../login/register-page/register-page.component';
 import { NavigationService } from 'src/app/services/navigation/navigation.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { Auth } from 'src/app/models/auth/auth-model';
+import { User } from 'src/app/models/user/user-model';
+import { Credentials } from 'src/app/models/credentials/credentials-model';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
 
-  constructor(private modalService: NgbModal, private navigationService: NavigationService) { }
+  profile: User | null = null;
+
+  constructor(
+    private modalService: NgbModal,
+    private navigationService: NavigationService,
+    private authService: AuthService
+    ) { }
+
+
+  ngOnInit(): void {
+    this.getProfile();
+  }
+
 
   onClickLogIn() {
     const modalRef = this.modalService.open(LoginPageComponent, { fullscreen: true });
@@ -28,4 +44,23 @@ export class NavbarComponent {
   onClickNavigateToCatalogue() {
     this.navigationService.navigateToCatalogue();
   }
+
+  getProfile() {
+    this.authService.getProfileListener()
+    .subscribe(user => {
+      this.profile = user;
+    });
+  }
+
+  onProfile(){
+    this.navigationService.navigateToProfile();
+  }
+
+  onLogout(){
+    //TODO LOGOUT
+    this.navigationService.navigateToHome();
+  }
+
+
 }
+
