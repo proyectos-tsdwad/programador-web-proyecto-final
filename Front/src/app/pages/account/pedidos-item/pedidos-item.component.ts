648744e55dfe5006c21217cac596e30dbab5ded2
nelsonvilte <nelsonvilte@gmail.com>
2023-05-23 22:27:49 -0300

    refactor the account-details component template and communication between components

--- Front/src/app/pages/account/pedidos-item/pedidos-item.component.ts
+++ Front/src/app/pages/account/pedidos-item/pedidos-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Purchase } from 'src/app/models/user/purchase-model';
 
 @Component({
   selector: 'app-pedidos-item',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./pedidos-item.component.css']
 })
 export class PedidosItemComponent {
-
+  @Input()
+  purchases!: Purchase[];
 }
