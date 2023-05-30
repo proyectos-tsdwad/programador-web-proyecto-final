31af0e5c4af90dee55f0434eb614293f9d737a13
carlos linero <lineroacostacarlos@gmail.com>
2023-05-29 22:27:18 -0300

    add join in book query

--- Back/Scripts/Libreria/libreria_franklin/serializers.py
+++ Back/Scripts/Libreria/libreria_franklin/serializers.py
@@ -1,25 +1,28 @@
 from rest_framework import serializers
 from .models import Book, Author, Publisher, Genre
 
-class BookSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = '__all__'
-
-class AuthorSerializers(serializers.ModelSerializer):
+class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = '__all__'
         read_only_fields = ('id_author', )       
 
-class PublisherSerializers(serializers.ModelSerializer):
+class PublisherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Publisher
         fields = '__all__'
         read_only_fields = ('id_publisher', )
 
-class GenreSerializers(serializers.ModelSerializer):
+class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
         fields = '__all__'
         read_only_fields = ('id_genre', )                     
+
+class BookSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer()
+    publisher = PublisherSerializer()
+    genre = GenreSerializer()
+    class Meta:
+        model = Book
+        fields = '__all__'
