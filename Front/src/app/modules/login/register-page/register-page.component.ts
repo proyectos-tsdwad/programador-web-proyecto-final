b5929c285b5f65913d9fad074c2b2e5461af53f7
carlariossuarez <carlaariossuarez@gmail.com>
2023-06-04 00:18:44 -0300

    add validations for register form

--- Front/src/app/modules/login/register-page/register-page.component.ts
+++ Front/src/app/modules/login/register-page/register-page.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { LoginPageComponent } from '../login-page/login-page.component';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { UserService } from 'src/app/services/user/user.service';
 import { Auth } from 'src/app/models/auth/auth-model';
 import { CreateUserDTO } from 'src/app/models/user/user-model';
 import { tap, catchError} from 'rxjs/operators';
 import { ROLE } from 'src/app/utils/enums/user.enum';
+import { regExEmail, regExOnlyNumbers, regExPassword } from 'src/app/utils/regex/regex';
 
 @Component({
   selector: 'app-register-page',
@@ -16,6 +17,58 @@ import { ROLE } from 'src/app/utils/enums/user.enum';
 export class RegisterPageComponent implements OnInit {
 
   registerForm!: FormGroup;
+
+  errorMessages = {
+    name: [
+      { type: 'required', message: 'Campo requerido.' },
+      { type: 'maxlength', message: 'Por favor ingresá un máximo de 80 caracteres.' }
+    ],
+    document: [
+      { type: 'required', message: 'Campo requerido.' },
+      { type: 'minlength', message: 'Mínimo 7 dígitos' },
+      { type: 'maxlength', message: 'Máximo 8 dígitos.' },
+      { type: 'pattern', message: 'Ingresa sólo números.' }
+    ],
+    areaCode: [
+      { type: 'required', message: 'Campo requerido.' },
+      { type: 'minlength', message: 'Mínimo 2 dígitos' },
+      { type: 'maxlength', message: 'Máximo 5 dígitos.' },
+      { type: 'pattern', message: 'Ingresa sólo números.' }
+    ],
+    telephone: [
+      { type: 'required', message: 'Campo requerido.' },
+      { type: 'minlength', message: 'Mínimo 6 dígitos' },
+      { type: 'maxlength', message: 'Máximo 8 dígitos.' },
+      { type: 'pattern', message: 'Ingresa sólo números.' }
+    ],
+    location: [
+      { type: 'required', message: 'Campo requerido.' },
+      { type: 'maxlength', message: 'Por favor ingresá un máximo de 50 caracteres.' }
+    ],
+    province: [
+      { type: 'required', message: 'Campo requerido.' },
+      { type: 'maxlength', message: 'Por favor ingresá un máximo de 50 caracteres.' }
+    ],
+    address: [
+      { type: 'required', message: 'Campo requerido.' },
+      { type: 'maxlength', message: 'Por favor ingresá un máximo de 80 caracteres.' }
+    ],
+    postalCode: [
+      { type: 'required', message: 'Campo requerido.' },
+      { type: 'minlength', message: 'Mínimo 4 dígitos' },
+      { type: 'maxlength', message: 'Máximo 4 dígitos.' },
+      { type: 'pattern', message: 'Ingresa sólo números.' }
+    ],
+    email: [
+      { type: 'required', message: 'Campo requerido.' },
+      { type: 'maxlength', message: 'Por favor ingresá un máximo de 80 caracteres.' },
+      { type: 'pattern', message: 'Ingresa un email válido.' }
+    ],
+    password: [
+      { type: 'required', message: 'Campo requerido.' },
+      { type: 'pattern', message: 'Debe contener al menos una letra mayúscula o minúscula, al menos un dígito y tener una longitud mínima de 8 caracteres'}
+    ],
+  };
 
   constructor(
     public activeModal: NgbActiveModal,
@@ -33,50 +86,104 @@ export class RegisterPageComponent implements OnInit {
 
   createForm() {
     this.registerForm = this.formBuilder.group({
-      name: [''],
-      email: [''],
-      password: [''],
-      areaCode: [''],
-      telephone: [''],
-      document: [''],
-      location: [''],
-      province: [''],
-      address: [''],
+      name: ['', [Validators.required, Validators.maxLength(80)]],
+      email: ['', [Validators.required, Validators.maxLength(80), Validators.pattern(regExEmail)]],
+      password: ['', [Validators.required, Validators.pattern(regExPassword)]],
+      areaCode: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(5), Validators.pattern(regExOnlyNumbers)]],
+      telephone: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(8), Validators.pattern(regExOnlyNumbers)]],
+      document: ['',[ Validators.required, Validators.minLength(7), Validators.maxLength(8), Validators.pattern(regExOnlyNumbers)]],
+      location: ['', [Validators.required, Validators.maxLength(50)]],
+      province: ['', [Validators.required, Validators.maxLength(50)]],
+      address: ['', [Validators.required, Validators.maxLength(80)]],
+      postalCode: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(4), Validators.pattern(regExOnlyNumbers)]]
     });
   }
 
-  registerUser(){
-    const userDto = this.getUserDto();
-    this.userService.createUser(userDto)
-    .pipe(
-      tap(response => {
-        console.log('Usuario registrado', response);
-        this.onClickRegister();
-      }),
-      catchError(error => {
-        console.log('Error al registrar', error);
-        throw error;
-      })
-    )
-    .subscribe();
+//   registerUser(){
+//     const userDto = this.getUserDto();
+//     this.userService.createUser(userDto)
+//     .pipe(
+//       tap(response => {
+//         console.log('Usuario registrado', response);
+//         this.onClickRegister();
+//       }),
+//       catchError(error => {
+//         console.log('Error al registrar', error);
+//         throw error;
+//       })
+//     )
+//     .subscribe();
    
+// }
+
+registerUser(event: Event){
+  event.preventDefault;
+  
+  if(this.registerForm.valid){
+    console.log('valido');
+    
+  } else{
+
+    this.registerForm.markAllAsTouched
+  }
+}
+
+get name() {
+  return this.registerForm.get('name');
+}
+
+get document() {
+  return this.registerForm.get('document');
+}
+
+get areaCode() {
+  return this.registerForm.get('areaCode');
+}
+
+get telephone() {
+  return this.registerForm.get('telephone');
+}
+
+get location() {
+  return this.registerForm.get('location');
+}
+
+get province() {
+  return this.registerForm.get('province');
+}
+
+get address() {
+  return this.registerForm.get('address');
+}
+
+get postalCode() {
+  return this.registerForm.get('postalCode');
+}
+
+get email() {
+  return this.registerForm.get('email');
+}
+
+get password() {
+  return this.registerForm.get('password');
 }
 
 
-  getUserDto(): CreateUserDTO  {
-    return {
-      name: this.registerForm.value.name,
-      email: this.registerForm.value.email,
-      password: this.registerForm.value.password,
-      areaCode: this.registerForm.value.areaCode,
-      telephone: this.registerForm.value.telephone,
-      document: this.registerForm.value.document,
-      location: this.registerForm.value.location,
-      province: this.registerForm.value.province,
-      address: this.registerForm.value.address,
-      roleId: ROLE.CLIENT
-    };
-  }
+  // getUserDto(): CreateUserDTO  {
+  //   return {
+  //     name: this.registerForm.value.name,
+  //     email: this.registerForm.value.email,
+  //     password: this.registerForm.value.password,
+  //     areaCode: this.registerForm.value.areaCode,
+  //     telephone: this.registerForm.value.telephone,
+  //     document: this.registerForm.value.document,
+  //     location: this.registerForm.value.location,
+  //     province: this.registerForm.value.province,
+  //     address: this.registerForm.value.address,
+  //     postalCode: this.registerForm.value.postalCode,
+  //     roleId: ROLE.CLIENT
+  //   };
+  // }
 
   onClickRegister() {
     this.activeModal.close();
