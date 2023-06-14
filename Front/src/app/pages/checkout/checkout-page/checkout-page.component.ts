6ff76ed052ee9df65922850d811c8bf823d20819
carlariossuarez <carlaariossuarez@gmail.com>
2023-06-14 20:02:37 -0300

    add validations for checkout-forms

--- Front/src/app/pages/checkout/checkout-page/checkout-page.component.ts
+++ Front/src/app/pages/checkout/checkout-page/checkout-page.component.ts
@@ -13,6 +13,7 @@ import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { CdkStepperModule } from '@angular/cdk/stepper';
 import { MatStepperModule } from '@angular/material/stepper';
 import { NavigationService } from 'src/app/services/navigation/navigation.service';
+import { regExEmail, regExOnlyNumbers } from 'src/app/utils/regex/regex';
 
 @Component({
   selector: 'app-checkout-page',
@@ -20,11 +21,59 @@ import { NavigationService } from 'src/app/services/navigation/navigation.servic
   styleUrls: ['./checkout-page.component.css'],
 })
 export class CheckoutPageComponent {
-  firstFormGroup: FormGroup;
+  firstFormGroup!: FormGroup;
   secondFormGroup: FormGroup;
   thirdFormGroup: FormGroup;
   stepperOrientation: Observable<StepperOrientation>;
   icons = ['./assets/img/stepper/Camion.png', 'icono-2', 'icono-3'];
+
+  errorMessages = {
+    name: [
+      { type: 'required', message: 'Campo requerido.' },
+      { type: 'maxlength', message: 'Por favor ingresá un máximo de 80 caracteres.' }
+    ],
+    document: [
+      { type: 'required', message: 'Campo requerido.' },
+      { type: 'minlength', message: 'Mínimo 7 dígitos' },
+      { type: 'maxlength', message: 'Máximo 8 dígitos.' },
+      { type: 'pattern', message: 'Ingresa sólo números.' }
+    ],
+    areaCode: [
+      { type: 'required', message: 'Campo requerido.' },
+      { type: 'minlength', message: 'Mínimo 2 dígitos' },
+      { type: 'maxlength', message: 'Máximo 5 dígitos.' },
+      { type: 'pattern', message: 'Ingresa sólo números.' }
+    ],
+    telephone: [
+      { type: 'required', message: 'Campo requerido.' },
+      { type: 'minlength', message: 'Mínimo 6 dígitos' },
+      { type: 'maxlength', message: 'Máximo 8 dígitos.' },
+      { type: 'pattern', message: 'Ingresa sólo números.' }
+    ],
+    location: [
+      { type: 'required', message: 'Campo requerido.' },
+      { type: 'maxlength', message: 'Por favor ingresá un máximo de 50 caracteres.' }
+    ],
+    province: [
+      { type: 'required', message: 'Campo requerido.' },
+      { type: 'maxlength', message: 'Por favor ingresá un máximo de 50 caracteres.' }
+    ],
+    address: [
+      { type: 'required', message: 'Campo requerido.' },
+      { type: 'maxlength', message: 'Por favor ingresá un máximo de 80 caracteres.' }
+    ],
+    postalCode: [
+      { type: 'required', message: 'Campo requerido.' },
+      { type: 'minlength', message: 'Mínimo 4 dígitos' },
+      { type: 'maxlength', message: 'Máximo 4 dígitos.' },
+      { type: 'pattern', message: 'Ingresa sólo números.' }
+    ],
+    email: [
+      { type: 'required', message: 'Campo requerido.' },
+      { type: 'maxlength', message: 'Por favor ingresá un máximo de 80 caracteres.' },
+      { type: 'pattern', message: 'Ingresa un email válido.' }
+    ]
+  };
 
   constructor(
     private _formBuilder: FormBuilder,
@@ -35,9 +84,7 @@ export class CheckoutPageComponent {
       .observe('(min-width: 800px)')
       .pipe(map(({ matches }) => (matches ? 'horizontal' : 'vertical')));
 
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required],
-    });
+    this.createFirstForm();
 
     this.secondFormGroup = this._formBuilder.group({
       secondCtrl: ['', Validators.required],
@@ -47,6 +94,57 @@ export class CheckoutPageComponent {
       thirdCtrl: ['', Validators.required],
     });
   }
+
+  createFirstForm(){
+    this.firstFormGroup = this._formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(80)]],
+      email: ['', [Validators.required, Validators.maxLength(80), Validators.pattern(regExEmail)]],
+      areaCode: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(5), Validators.pattern(regExOnlyNumbers)]],
+      telephone: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(8), Validators.pattern(regExOnlyNumbers)]],
+      document: ['',[ Validators.required, Validators.minLength(7), Validators.maxLength(8), Validators.pattern(regExOnlyNumbers)]],
+      location: ['', [Validators.required, Validators.maxLength(50)]],
+      province: ['', [Validators.required, Validators.maxLength(50)]],
+      address: ['', [Validators.required, Validators.maxLength(80)]],
+      postalCode: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(4), Validators.pattern(regExOnlyNumbers)]]
+    });
+  }
+
+  get name() {
+    return this.firstFormGroup.get('name');
+  }
+  
+  get document() {
+    return this.firstFormGroup.get('document');
+  }
+  
+  get areaCode() {
+    return this.firstFormGroup.get('areaCode');
+  }
+  
+  get telephone() {
+    return this.firstFormGroup.get('telephone');
+  }
+  
+  get location() {
+    return this.firstFormGroup.get('location');
+  }
+  
+  get province() {
+    return this.firstFormGroup.get('province');
+  }
+  
+  get address() {
+    return this.firstFormGroup.get('address');
+  }
+  
+  get postalCode() {
+    return this.firstFormGroup.get('postalCode');
+  }
+  
+  get email() {
+    return this.firstFormGroup.get('email');
+  }
+  
 
   onClickBackToCart() {
     this.navigationService.navigateToCartDetail();
