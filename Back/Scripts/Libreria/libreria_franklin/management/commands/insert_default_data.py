d5124128ef99845fca27b540fdac65d1617e9cbe
nelsonvilte <nelsonvilte@gmail.com>
2023-06-19 17:56:23 -0300

    add delivery, payment and sell default data

--- Back/Scripts/Libreria/libreria_franklin/management/commands/insert_default_data.py
+++ Back/Scripts/Libreria/libreria_franklin/management/commands/insert_default_data.py
@@ -109,3 +109,78 @@ class Command(BaseCommand):
         else:
             self.stdout.write('The Store tabe is not empty.')               
 
+
+        # Insertar instancias de Delivery
+        file_path = os.path.abspath('libreria_franklin/default data/delivery_data.json')
+        if not Delivery.objects.exists():
+            with open(file_path) as file:
+                data = json.load(file)
+            for delivery_data in data:
+                id_delivery = delivery_data['id_delivery']
+                name = delivery_data['name']
+                email = delivery_data['email']
+                telephone_number = delivery_data['telephone_number']
+                telephone_area_code = delivery_data['telephone_area_code']
+                document = delivery_data['document']
+                address_province = delivery_data['address_province']
+                address_location = delivery_data['address_location']
+                address_street = delivery_data['address_street']
+                postal_code = delivery_data['postal_code']
+                state = delivery_data['state']
+
+                Delivery.objects.create(id_delivery=id_delivery, name=name, email=email,
+                                        telephone_number=telephone_number, telephone_area_code=telephone_area_code,
+                                        document=document, address_province=address_province, address_location=address_location,
+                                        address_street=address_street, postal_code=postal_code, state=state)
+
+            self.stdout.write(self.style.SUCCESS('Default deliveries inserted successfully.'))
+        else:
+            self.stdout.write('The Delivery table is not empty.')    
+
+        # Insertar Instancias de Payment
+        file_path = os.path.abspath('libreria_franklin/default data/payment_data.json')
+        if not Payment.objects.exists():
+            with open(file_path) as file:
+                data = json.load(file)
+
+            for payment_data in data:
+                id_payment = payment_data['id_payment']
+                card_association = payment_data['card_association']
+                number = payment_data['number']
+                cvv = payment_data['cvv']
+                expiration = payment_data['expiration']
+
+                Payment.objects.create(id_payment=id_payment, card_association=card_association, number=number,
+                                    cvv=cvv, expiration=expiration)
+
+            self.stdout.write(self.style.SUCCESS('Default payments inserted successfully.'))
+        else:
+            self.stdout.write('The Payment table is not empty.')
+            
+         # Insertar instancias de Sell
+        file_path_sell = os.path.abspath('libreria_franklin/default data/sell_data.json')
+        if not Sell.objects.exists():
+            with open(file_path_sell) as file:
+                data = json.load(file)
+            for sell_data in data:
+                id_sell = sell_data['id_sell']
+                orderNumber = sell_data['orderNumber']
+                saleDate = sell_data['saleDate']
+                #products = sell_data['products']
+                deliveryType = sell_data['deliveryType']
+                paymentType = sell_data['paymentType']
+                totalQuantity = sell_data['totalQuantity']
+                totalCost = sell_data['totalCost']
+                profile = CustomUser.objects.get(id=sell_data['profileId'])
+                delivery = Delivery.objects.get(id_delivery=sell_data['deliveryId'])
+                payment = Payment.objects.get(id_payment=sell_data['paymentId'])
+                book = Book.objects.get(isbn=sell_data['bookId'])
+
+                Sell.objects.create(id_sell=id_sell, orderNumber=orderNumber, saleDate=saleDate,
+                                    deliveryType=deliveryType, paymentType=paymentType,
+                                    totalQuantity=totalQuantity, totalCost=totalCost, profile=profile,
+                                    delivery=delivery, payment=payment, book=book)
+
+            self.stdout.write(self.style.SUCCESS('Default sells inserted successfully.'))
+        else:
+            self.stdout.write('The Sell table is not empty.')
