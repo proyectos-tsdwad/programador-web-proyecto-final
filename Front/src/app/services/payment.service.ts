4535e70246dd458ca467fc15c0556e6ff25a5c10
carlos linero <lineroacostacarlos@gmail.com>
2023-06-20 01:22:50 -0300

    add payment service

---
+++ Front/src/app/services/payment.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentService {
+
+  private apiUrl = environment.API_URL;
+
+  constructor(private http: HttpClient) { }
+}
